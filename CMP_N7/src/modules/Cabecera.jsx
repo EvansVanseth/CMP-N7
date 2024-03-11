db96060cb4e4a7fda15931c2697b948fe4210967
Juan Alonso Garcia <evansvanseth@gmail.com>
2024-03-11 18:52:21 +0100

    modified:   CMP_N7/src/modules/Cabecera.jsx

--- CMP_N7/src/modules/Cabecera.jsx
+++ CMP_N7/src/modules/Cabecera.jsx
@@ -1,4 +1,5 @@
 import '../css/Cabecera.css'
+import appLogo from '../assets/app-logo.png'
 
 function Cabecera() {
   // const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function Cabecera() {
     <header className="app-header">
       <div className="app-header-in contenedor">
         <h1 className="app-title">Chaos Manager <span className="app-title-in">for</span> Players</h1>
-        <img className="app-logo" alt="logo de la aplicacion" src="./src/assets/app-logo.png"></img>
+        <img className="app-logo" alt="logo de la aplicacion" src={appLogo}></img>
       </div>
     </header>
   )
